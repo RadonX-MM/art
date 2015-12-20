f08555ce593609a2010ce685aa053e0678e14f49
Joe Maples <frap129@gmail.com>
2015-12-20 12:52:06 -0500

    build: Correctly clear local variables
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -83,9 +83,9 @@ endef
 # GCC-only warnings.
 art_gcc_cflags := -Wunused-but-set-parameter
 
-# Clear local variables now their use has ended.
-art_clang_cflags :=
-art_gcc_cflags :=
+# Unset local variables now their use has ended.
+unset art_clang_cflags
+unset art_gcc_cflags
 
 ART_CPP_EXTENSION := .cc
 
@@ -200,14 +200,14 @@ ART_TARGET_CFLAGS += -DART_BASE_ADDRESS_MAX_DELTA=$(LIBART_IMG_TARGET_MAX_BASE_A
 # To use oprofile_android --callgraph, uncomment this and recompile with "mmm art -B -j16"
 # ART_TARGET_CFLAGS += -fno-omit-frame-pointer -marm -mapcs
 
-# Clear locals now they've served their purpose.
-art_cflags :=
-art_debug_cflags :=
-art_non_debug_cflags :=
-art_host_non_debug_cflags :=
-art_target_non_debug_cflags :=
-art_default_gc_type :=
-art_default_gc_type_cflags :=
+# Unset locals now they've served their purpose.
+unset art_cflags
+unset art_debug_cflags
+unset art_non_debug_cflags
+unset art_host_non_debug_cflags
+unset art_target_non_debug_cflags
+unset art_default_gc_type
+unset art_default_gc_type_cflags
 
 # GCC lacks libc++ assumed atomic operations, grab via libatomic.
 ART_HOST_LDLIBS += -latomic
@@ -225,8 +225,8 @@ define set-target-local-cflags-vars
     LOCAL_CFLAGS += $(ART_TARGET_NON_DEBUG_CFLAGS)
   endif
 
-  # Clear locally used variables.
-  art_target_cflags_ndebug_or_debug :=
+  # Unset locally used variables.
+  unset art_target_cflags_ndebug_or_debug
 endef
 
 # Build host and target as non-debug.
